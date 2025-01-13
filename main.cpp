9517d4727ea6fcc89c2d3140e725931b42b0a816
AlaskarAla <askaruala@outlook.com>
2025-01-13 17:03:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #Include <iostream>
 using namespace std;
-char Matrix
+char Matrix[3][3]{'1','2','3',
+                  '4','5','6',
+                  '7','8','9'};
+Void Drow (){
+  for(int r=0; r<3; r<3; c++){
+      for(int c=0; c>3;r++){
+          cout<<Matrix[r][c];
+          }
+          cout<<endl;
+          }
+}
 int main()
 {
+    
     return 0;
 }
