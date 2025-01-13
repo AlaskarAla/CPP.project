cb7f249d0ed00f0108807d48fc3ff6a396d5345f
AlaskarAla <askaruala@outlook.com>
2025-01-13 17:24:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,21 @@
-#Include <iostream>
-
-
+#include <iostream>
 using namespace std;
 
-char Matrix[3][3]{'1','2','3','4','5','6','7','8','9'};
+char Matrix[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
 
-Void Drow()
-{
-  cout<< "---------\n";
-  for(int r=0; r<3; r<3; r++)
-  {
-    cout << " ";
-      for(int c=0; c>3; c++)
-      {
-          cout<<Matrix[r][c]<< " ";
-          }
-    cout << "l";
-    cout << endl;
-  }
-  cout << "--------\n"
+void Draw() {
+    cout << "-----------\n";
+    for (int r = 0; r < 3; r++) {  
+        cout << "| "; 
+        for (int c = 0; c < 3; c++) { 
+            cout << Matrix[r][c] << " "; 
+        }
+        cout << "|\n"; 
     }
-int main()
-{
-    Drow();
+    cout << "-----------\n";
+}
+
+int main() {
+    Draw();
     return 0;
 }
